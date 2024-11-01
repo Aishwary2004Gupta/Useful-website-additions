088db650b8d19163b157dee8a1f059e2827056b3
Aishwary Gupta <aish042004@gmail.com>
2024-11-01 23:41:17 +0530

    Update script.js

---
+++ Dark-Light-mode-toggle/script.js
@@ -0,0 +1,9 @@
+$(".change").on("click", function() {
+    if ($("body").hasClass("dark")) {
+        $("body").removeClass("dark");
+        $(".change").html("&#9728;"); // Sun Icon
+    } else {
+        $("body").addClass("dark");
+        $(".change").html("&#9790;"); // Moon Icon
+    }
+});

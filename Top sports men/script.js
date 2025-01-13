bfe791486eb6f0dbf18359d98d7e9c606bfaf140
Aishwary Gupta <aish042004@gmail.com>
2025-01-13 22:50:50 +0530

    Update script.js

--- Top sports men/script.js
+++ Top sports men/script.js
@@ -1,129 +1,129 @@
 const IMAGES = [
     {
-      "id": "image-1",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/M/MAXVER01_Max_Verstappen/maxver01.png",
-      "alt": "Max Verstappen",
-      "title": "Max Verstappen"
+        "id": "image-1",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/M/MAXVER01_Max_Verstappen/maxver01.png",
+        "alt": "Max Verstappen",
+        "title": "Max Verstappen"
     },
     {
-      "id": "image-2",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/C/CHALEC01_Charles_Leclerc/chalec01.png",
-      "alt": "Charles Leclerc",
-      "title": "Charles Leclerc"
+        "id": "image-2",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/C/CHALEC01_Charles_Leclerc/chalec01.png",
+        "alt": "Charles Leclerc",
+        "title": "Charles Leclerc"
     },
     {
-      "id": "image-3",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LANNOR01_Lando_Norris/lannor01.png",
-      "alt": "Lando Norris",
-      "title": "Lando Norris"
+        "id": "image-3",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LANNOR01_Lando_Norris/lannor01.png",
+        "alt": "Lando Norris",
+        "title": "Lando Norris"
     },
     {
-      "id": "image-4",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/C/CARSAI01_Carlos_Sainz/carsai01.png",
-      "alt": "Carlos Sainz",
-      "title": "Carlos Sainz"
+        "id": "image-4",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/C/CARSAI01_Carlos_Sainz/carsai01.png",
+        "alt": "Carlos Sainz",
+        "title": "Carlos Sainz"
     },
     {
-      "id": "image-5",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/S/SERPER01_Sergio_Perez/serper01.png",
-      "alt": "Sergio Perez",
-      "title": "Sergio Perez"
+        "id": "image-5",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/S/SERPER01_Sergio_Perez/serper01.png",
+        "alt": "Sergio Perez",
+        "title": "Sergio Perez"
     },
     {
-      "id": "image-6",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/O/OSCPIA01_Oscar_Piastri/oscpia01.png",
-      "alt": "Oscar Piastre",
-      "title": "Oscar Piastre"
+        "id": "image-6",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/O/OSCPIA01_Oscar_Piastri/oscpia01.png",
+        "alt": "Oscar Piastre",
+        "title": "Oscar Piastre"
     },
     {
-      "id": "image-7",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/G/GEORUS01_George_Russell/georus01.png",
-      "alt": "George Russel",
-      "title": "George Russel"
+        "id": "image-7",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/G/GEORUS01_George_Russell/georus01.png",
+        "alt": "George Russel",
+        "title": "George Russel"
     },
     {
-      "id": "image-8",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LEWHAM01_Lewis_Hamilton/lewham01.png",
-      "alt": "Lewis Hamilton",
-      "title": "Lewis Hamilton"
+        "id": "image-8",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LEWHAM01_Lewis_Hamilton/lewham01.png",
+        "alt": "Lewis Hamilton",
+        "title": "Lewis Hamilton"
     },
     {
-      "id": "image-9",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/F/FERALO01_Fernando_Alonso/feralo01.png",
-      "alt": "Fernando Alonso",
-      "title": "Fernando Alonso"
+        "id": "image-9",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/F/FERALO01_Fernando_Alonso/feralo01.png",
+        "alt": "Fernando Alonso",
+        "title": "Fernando Alonso"
     },
     {
-      "id": "image-10",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/Y/YUKTSU01_Yuki_Tsunoda/yuktsu01.png",
-      "alt": "Yuki Tsunoda",
-      "title": "Yuki Tsunoda"
+        "id": "image-10",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/Y/YUKTSU01_Yuki_Tsunoda/yuktsu01.png",
+        "alt": "Yuki Tsunoda",
+        "title": "Yuki Tsunoda"
     },
     {
-      "id": "image-11",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LANSTR01_Lance_Stroll/lanstr01.png",
-      "alt": "Lance Stroll",
-      "title": "Lance Stroll"
+        "id": "image-11",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LANSTR01_Lance_Stroll/lanstr01.png",
+        "alt": "Lance Stroll",
+        "title": "Lance Stroll"
     },
     {
-      "id": "image-12",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/D/DANRIC01_Daniel_Ricciardo/danric01.png",
-      "alt": "Daniel Riccardo",
-      "title": "Daniel Riccardo"
+        "id": "image-12",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/D/DANRIC01_Daniel_Ricciardo/danric01.png",
+        "alt": "Daniel Riccardo",
+        "title": "Daniel Riccardo"
     },
     {
-      "id": "image-13",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/O/OLIBEA01_Oliver_Bearman/olibea01.png",
-      "alt": "Oliver Bearman",
-      "title": "Oliver Bearman"
+        "id": "image-13",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/O/OLIBEA01_Oliver_Bearman/olibea01.png",
+        "alt": "Oliver Bearman",
+        "title": "Oliver Bearman"
     },
     {
-      "id": "image-14",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/N/NICHUL01_Nico_Hulkenberg/nichul01.png",
-      "alt": "Nico Hulkenberg",
-      "title": "Nico Hulkenberg"
+        "id": "image-14",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/N/NICHUL01_Nico_Hulkenberg/nichul01.png",
+        "alt": "Nico Hulkenberg",
+        "title": "Nico Hulkenberg"
     },
     {
-      "id": "image-15",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/P/PIEGAS01_Pierre_Gasly/piegas01.png",
-      "alt": "Pierre Gasly",
-      "title": "Pierre Gasly"
+        "id": "image-15",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/P/PIEGAS01_Pierre_Gasly/piegas01.png",
+        "alt": "Pierre Gasly",
+        "title": "Pierre Gasly"
     },
     {
-      "id": "image-16",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/A/ALEALB01_Alexander_Albon/alealb01.png",
-      "alt": "Alexander Albon",
-      "title": "Alexander Albon"
+        "id": "image-16",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/A/ALEALB01_Alexander_Albon/alealb01.png",
+        "alt": "Alexander Albon",
+        "title": "Alexander Albon"
     },
     {
-      "id": "image-17",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/E/ESTOCO01_Esteban_Ocon/estoco01.png",
-      "alt": "Eseban Ocon",
-      "title": "Eseban Ocon"
+        "id": "image-17",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/E/ESTOCO01_Esteban_Ocon/estoco01.png",
+        "alt": "Eseban Ocon",
+        "title": "Eseban Ocon"
     },
     {
-      "id": "image-18",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/K/KEVMAG01_Kevin_Magnussen/kevmag01.png",
-      "alt": "Kevin Magnussen",
-      "title": "Kevin Magnussen"
+        "id": "image-18",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/K/KEVMAG01_Kevin_Magnussen/kevmag01.png",
+        "alt": "Kevin Magnussen",
+        "title": "Kevin Magnussen"
     },
     {
-      "id": "image-19",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/G/GUAZHO01_Guanyu_Zhou/guazho01.png",
-      "alt": "Guanyu Zhou",
-      "title": "Guanyu Zhou"
+        "id": "image-19",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/G/GUAZHO01_Guanyu_Zhou/guazho01.png",
+        "alt": "Guanyu Zhou",
+        "title": "Guanyu Zhou"
     },
     {
-      "id": "image-20",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/V/VALBOT01_Valtteri_Bottas/valbot01.png",
-      "alt": "Valtteri Botas",
-      "title": "Valtteri Botas"
+        "id": "image-20",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/V/VALBOT01_Valtteri_Bottas/valbot01.png",
+        "alt": "Valtteri Botas",
+        "title": "Valtteri Botas"
     },
     {
-      "id": "image-21",
-      "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LOGSAR01_Logan_Sargeant/logsar01.png",
-      "alt": "Logan Sargeant",
-      "title": "Logan Sargeant"
+        "id": "image-21",
+        "src": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LOGSAR01_Logan_Sargeant/logsar01.png",
+        "alt": "Logan Sargeant",
+        "title": "Logan Sargeant"
     }
 ];
 

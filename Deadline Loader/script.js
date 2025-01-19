d146977f7780bdfd0ce4a10c0e31509508f3303b
Aishwary Gupta <aish042004@gmail.com>
2025-01-19 16:28:20 +0530

    Update script.js

--- Deadline Loader/script.js
+++ Deadline Loader/script.js
@@ -1,22 +1,54 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const maskRed = document.querySelector(".mask-red");
-    const deadlineText = document.querySelector(".deadline-text");
-  
-    let days = 7;
-  
-    function startAnimation() {
-      maskRed.style.transform = "translateX(0)";
-      const interval = setInterval(() => {
-        if (days > 1) {
-          days--;
-          deadlineText.textContent = `Deadline ${days}`;
-        } else {
-          clearInterval(interval);
-          deadlineText.textContent = "Deadline Over!";
+var $ = jQuery;
+var animationTime = 20,
+    days = 7;
+$(document).ready(function () {
+    $('#progress-time-fill, #death-group').css({ 'animation-duration': animationTime + 's' });
+
+
+    var deadlineAnimation = function () {
+        setTimeout(function () {
+            $('#designer-arm-grop').css({ 'animation-duration': '1.5s' });
+        }, 0);
+        setTimeout(function () {
+            $('#designer-arm-grop').css({ 'animation-duration': '1s' });
+        }, 4000);
+        setTimeout(function () {
+            $('#designer-arm-grop').css({ 'animation-duration': '0.7s' });
+        }, 8000);
+        setTimeout(function () {
+            $('#designer-arm-grop').css({ 'animation-duration': '0.3s' });
+        }, 12000);
+        setTimeout(function () {
+            $('#designer-arm-grop').css({ 'animation-duration': '0.2s' });
+        }, 15000);
+    };
+    function timer(totalTime, deadline) {
+        var time = totalTime * 1000;
+        var dayDuration = time / deadline;
+        var actualDay = deadline;
+        var timer = setInterval(countTime, dayDuration);
+        function countTime() {
+            --actualDay;
+            $('.deadline-days .day').text(actualDay);
+
+
+            if (actualDay == 0) {
+                clearInterval(timer);
+                $('.deadline-days .day').text(deadline);
+            }
         }
-      }, 1000);
     }
-  
-    startAnimation();
-  });
-  
+    var deadlineText = function () {
+        var $el = $('.deadline-days');
+        var html = '<div class="mask-red"><div class="inner">' + $el.html() + '</div></div><div class="mask-white"><div class="inner">' + $el.html() + '</div></div>';
+        $el.html(html);
+    }
+    deadlineText();
+    deadlineAnimation();
+    timer(animationTime, days);
+    setInterval(function () {
+        timer(animationTime, days);
+        deadlineAnimation();
+        console.log('begin interval', animationTime * 1000);
+    }, animationTime * 1000);
+});

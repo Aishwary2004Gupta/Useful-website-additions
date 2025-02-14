3acbae8f9657e3f4362972862fac3652766ff436
Aishwary Gupta <aish042004@gmail.com>
2025-02-14 23:03:55 +0530

    Update script.js

--- ImageCreation/script.js
+++ ImageCreation/script.js
@@ -8,6 +8,6 @@ allPaths.forEach((path) => {
 
     path.getBoundingClientRect();
 
-    path.style.transition = "stroke-dashoffset 2s ease";
+    path.style.transition = "stroke-dashoffset 3s ease-in-out";
     path.style.strokeDashoffset = "0";
 });

d7f8ec9fd0f462f876834845654db42f066cb194
Aishwary Gupta <aish042004@gmail.com>
2025-04-08 23:25:55 +0530

    Update script.js

--- Hover Flare/script.js
+++ Hover Flare/script.js
@@ -7,7 +7,7 @@ canvas.height = window.innerHeight;
 
 let particles = [];
 const mouse = { x: undefined, y: undefined };
-let theme = "light"; 
+let theme = "light";
 
 // Particle class
 class Particle {

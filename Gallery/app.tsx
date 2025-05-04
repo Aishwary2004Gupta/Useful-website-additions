eebb04f89641b6e0ef13190a5092d9d2463b78e2
Aishwary Gupta <aish042004@gmail.com>
2025-05-04 15:32:55 +0530

    Update app.tsx

--- Gallery/app.tsx
+++ Gallery/app.tsx
@@ -4,7 +4,12 @@ import "./style.css";
 function App() {
     return(
         <main>
-            <div className="progress"> </div> 
+            <div className="progress"> </div>
+            {[ ...Array(10) ].map((arr) => (
+                <div className="card">
+                    This is My photo gallery
+                </div>
+            ))} 
         </main>
     )
 }

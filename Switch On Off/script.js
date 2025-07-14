0b6524d3d671e192d884ec3ecb2bce4f7c897b59
Aishwary Gupta <aish042004@gmail.com>
2025-07-14 23:23:17 +0530

    Update script.js

--- Switch On Off/script.js
+++ Switch On Off/script.js
@@ -1,8 +1,8 @@
 //just for the background 
-document.querySelector('.switch input').addEventListener('change', function(e) {
-  if (e.target.checked) {
-    document.body.classList.remove('switch-off');
-  } else {
-    document.body.classList.add('switch-off');
-  }
+document.querySelector('.switch input').addEventListener('change', function (e) {
+    if (e.target.checked) {
+        document.body.classList.remove('switch-off');
+    } else {
+        document.body.classList.add('switch-off');
+    }
 });

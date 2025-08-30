728972c2898a4a51ded06f2db66af7541be2f5fd
Aishwary Gupta <aish042004@gmail.com>
2025-08-30 23:39:07 +0530

    Update script.js

--- Motion Track Path/script.js
+++ Motion Track Path/script.js
@@ -1,21 +1,18 @@
-import anime from 'https://esm.sh/animejs';
+import { animate, svg } from "https://esm.sh/animejs";
 
-// Animate the transforms properties of .car along the motion path
-const motionPath = anime({
-  targets: '.car',
-  translateX: anime.path('path'),
-  translateY: anime.path('path'),
-  rotate: anime.path('path'),
-  easing: 'linear',
+// Animate the transforms properties of .car the motion path values
+const carAnimation = animate('.car', {
+  ease: 'linear',
   duration: 5000,
-  loop: true
+  loop: true,
+  ...svg.createMotionPath('path')
 });
 
-// Line drawing animation
-anime({
-  targets: 'path',
-  strokeDashoffset: [anime.setDashoffset, 0],
-  easing: 'linear',
+// Line drawing animation following the motion path values
+// For demo aesthetic only
+animate(svg.createDrawable('path'), {
+  draw: '0 1',
+  ease: 'linear',
   duration: 5000,
   loop: true
 });

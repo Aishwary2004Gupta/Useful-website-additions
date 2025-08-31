40715c1a767fa30b6ba787f89eac4d8a8b862f33
Aishwary Gupta <aish042004@gmail.com>
2025-08-31 23:46:51 +0530

    Create script.js

---
+++ Hover to animate/script.js
@@ -0,0 +1,33 @@
+import { animate, utils, stagger, text } from "https://esm.sh/animejs";
+// import { animate, utils, stagger, text } from 'animejs';
+
+const colors = [];
+
+text.split('p', {
+  lines: true,
+})
+/* Registering an animation to the split */
+.addEffect(({ lines }) => animate(lines, {
+  y: ['50%', '-50%'],
+  loop: true,
+  alternate: true,
+  delay: stagger(400),
+  ease: 'inOutQuad',
+}))
+/* Registering a callback to the split */
+.addEffect(split => {
+  split.words.forEach(($el, i) => {
+    const color = colors[i];
+    if (color) utils.set($el, { color });
+    $el.addEventListener('pointerenter', () => {
+      animate($el, {
+        color: utils.randomPick(['#FF4B4B', '#FFCC2A', '#B7FF54', '#57F695']),
+        duration: 250,
+      })
+    });
+  });
+  return () => {
+    /* Called between each split */
+    split.words.forEach((w, i) => colors[i] = utils.get(w, 'color'));
+  }
+});

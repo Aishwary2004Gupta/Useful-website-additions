e338ad028d1ac1251e74af6b27deab4c45a8f3b4
Aishwary Gupta <aish042004@gmail.com>
2025-09-07 01:11:22 +0530

    Update script.js

--- ASCII Image/script.js
+++ ASCII Image/script.js
@@ -62,12 +62,15 @@ function print(config) {
   let original = new Image();
   original.crossOrigin = 'Anonymous';
   original.onload = function () {
-    let dataCtx = document.createElement('canvas').getContext('2d');
-    config.canvas.width = dataCtx.canvas.width = this.width;
-    config.canvas.height = dataCtx.canvas.height = this.height;
+    const targetWidth = 600;
+    const targetHeight = 600;
 
-    dataCtx.drawImage(this, 0, 0, this.width / config.spaceing, this.height / config.spaceing);
-    let data = dataCtx.getImageData(0, 0, original.width, original.height).data;
+    let dataCtx = document.createElement('canvas').getContext('2d');
+    config.canvas.width = dataCtx.canvas.width = targetWidth;
+    config.canvas.height = dataCtx.canvas.height = targetHeight;
+
+    dataCtx.drawImage(this, 0, 0, targetWidth / config.spaceing, targetHeight / config.spaceing);
+    let data = dataCtx.getImageData(0, 0, targetWidth, targetHeight).data;
 
     let ctx = config.canvas.getContext('2d');
     ctx.fillStyle = '#fff';
@@ -75,8 +78,8 @@ function print(config) {
     let represenation = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^'. ";
 
     for (let i = 0, ii = 0; i < data.length; i += 4, ii++) {
-      let x = ii % this.width;
-      let y = ii / this.width | 0;
+      let x = ii % targetWidth;
+      let y = ii / targetWidth | 0;
       let grayscale = (data[i] + data[i + 1] + data[i + 2]) / 3 | 0;
       let char = represenation[map(grayscale, 255, 0, 0, represenation.length - 1) | 0];
 

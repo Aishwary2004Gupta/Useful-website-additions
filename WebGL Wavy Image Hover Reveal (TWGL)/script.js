37b0ee5c6eed0ad720f1a1431a37a0ed9780666b
Aishwary Gupta <aish042004@gmail.com>
2026-01-14 22:35:54 +0530

    Update script.js

--- WebGL Wavy Image Hover Reveal (TWGL)/script.js
+++ WebGL Wavy Image Hover Reveal (TWGL)/script.js
@@ -53,7 +53,7 @@ class Ting {
             {
                 darkTexture: {
                     src:
-                        "https://images.unsplash.com/photo-1712351112725-de8352955385?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        "http://images.unsplash.com/photo-1560184776-4335046f4feb?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 }
             },
             (err, textures, sources) => {

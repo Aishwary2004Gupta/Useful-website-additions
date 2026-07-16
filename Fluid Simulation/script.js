5d6122be5293d3aa4b95adbb5cdcdf978d065c66
Aishwary Gupta <aish042004@gmail.com>
2026-07-16 22:40:08 +0530

    Update script.js

--- Fluid Simulation/script.js
+++ Fluid Simulation/script.js
@@ -32,33 +32,33 @@ import {
 //         gl_FragColor = vec4(0.0);
 //         return;
 //     }
-    
+
 //     vec4 data = texture2D(textureA, uv);
 //     float pressure = data.x;
 //     float pVel = data.y;
-    
+
 //     vec2 texelSize = 1.0 / resolution;
 //     float p_right = texture2D(textureA, uv + vec2(texelSize.x, 0.0)).x;
 //     float p_left = texture2D(textureA, uv + vec2(-texelSize.x, 0.0)).x;
 //     float p_up = texture2D(textureA, uv + vec2(0.0, texelSize.y)).x;
 //     float p_down = texture2D(textureA, uv + vec2(0.0, -texelSize.y)).x;
-    
+
 //     if (uv.x <= texelSize.x) p_left = p_right;
 //     if (uv.x >= 1.0 - texelSize.x) p_right = p_left;
 //     if (uv.y <= texelSize.y) p_down = p_up;
 //     if (uv.y >= 1.0 - texelSize.y) p_up = p_down;
-    
+
 //     // Enhanced wave equation matching ShaderToy
 //     pVel += delta * (-2.0 * pressure + p_right + p_left) / 4.0;
 //     pVel += delta * (-2.0 * pressure + p_up + p_down) / 4.0;
-    
+
 //     pressure += delta * pVel;
-    
+
 //     pVel -= 0.005 * delta * pressure;
-    
+
 //     pVel *= 1.0 - 0.002 * delta;
 //     pressure *= 0.999;
-    
+
 //     vec2 mouseUV = mouse / resolution;
 //     if(mouse.x > 0.0) {
 //         float dist = distance(uv, mouseUV);
@@ -66,7 +66,7 @@ import {
 //             pressure += 2.0 * (1.0 - dist / 0.02);  // Increased intensity
 //         }
 //     }
-    
+
 //     gl_FragColor = vec4(pressure, pVel, 
 //         (p_right - p_left) / 2.0, 
 //         (p_up - p_down) / 2.0);
@@ -88,14 +88,14 @@ import {
 
 // void main() {
 //     vec4 data = texture2D(textureA, vUv);
-    
+
 //     vec2 distortion = 0.3 * data.zw;
 //     vec4 color = texture2D(textureB, vUv + distortion);
-    
+
 //     vec3 normal = normalize(vec3(-data.z * 2.0, 0.5, -data.w * 2.0));
 //     vec3 lightDir = normalize(vec3(-3.0, 10.0, 3.0));
 //     float specular = pow(max(0.0, dot(normal, lightDir)), 60.0) * 1.5;
-    
+
 //     gl_FragColor = color + vec4(specular);
 // }
 // `
